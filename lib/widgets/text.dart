cf527b70d1fef104c8cb6eca2bf72b705592e6dc
ahmed emara <emara8028@gmail.com>
2025-10-12 17:13:51 +0300

    refactor: Move extensions to a dedicated directory

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -7,50 +7,25 @@ class _PagifyText extends StatelessWidget {
 
   /// text color [Color]
   final Color? color;
-
-  /// text font size [double]
-  final double? fontSize;
-
-  /// text font weight [FontWeight]
-  final FontWeight fontWeight;
-
-  /// text align [TextAlign]
-  final TextAlign textAlign;
-
-  /// text overflow [TextOverflow]
-  final TextOverflow? overflow;
-
-  /// text max lines [int]
-  final int? maxLines;
-
-  /// text height [double]
-  final double? height;
+  final TextAlign? textAlign;
 
   /// pagify text widget constructor
   const _PagifyText(
     this.text, {
-    super.key,
-    this.color,
-    this.fontSize,
-    this.fontWeight = FontWeight.normal,
-    this.overflow,
-    this.textAlign = TextAlign.start,
-    this.maxLines,
-    this.height,
+    this.color, this.textAlign,
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      textAlign: textAlign,
-      overflow: overflow,
-      maxLines: maxLines,
+      textAlign: textAlign ?? TextAlign.start,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
       style: TextStyle(
         color: color,
-        fontSize: fontSize ?? 16,
-        fontWeight: fontWeight,
-        height: height,
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
       ),
     );
   }
